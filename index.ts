4f1d1fc1b2586008d25af75239450b4c94994d74
Anwesh43 <anweshthecool0@gmail.com>
2020-09-04 16:27:30 +0530

    adding BSLNode class

--- index.ts
+++ index.ts
@@ -145,3 +145,44 @@ class Animator {
         }
     }
 }
+
+class BSLNode {
+
+    prev : BSLNode
+    next : BSLNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBSRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BSLNode {
+        var curr : BSLNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}

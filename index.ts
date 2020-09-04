42ad16d3184919a79eb6d7b952b1fee12937b729
Anwesh43 <anweshthecool0@gmail.com>
2020-09-04 14:30:22 +0530

    adding constantsa

---
+++ index.ts
@@ -0,0 +1,9 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const parts : number = 4
+const scGap : number = 0.02 / parts
+const strokeFactor : number = 90
+const sizeFactor : number = 4.5
+const delay : number = 20
+const colors : Array<String> = ["#4CAF50", "#F44336", "#FF9800", "#2196F3", "#009688"]
+const backColor : string = "#bdbdbd"
